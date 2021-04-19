cf05eea04d212f11685361da6bfe8b307c3ccd97
manogna4 <manogna@gmail.com>
2021-04-19 07:59:32 +0530

    playing with vertical tabs

--- main.ts
+++ main.ts
@@ -1,116 +1,147 @@
-import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Modal, Notice, Plugin, PluginSettingTab, Setting, MarkdownView, Workspace} from 'obsidian';
+import { WorkspaceSplit, WorkspaceLeaf, WorkspaceItem } from 'obsidian';
+import { ItemView, Menu } from 'obsidian';
 
 const plugin_name = 'koncham-tinker'
 
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const view_type = 'root-leaves-list'
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	public view: RootLeavesListView;
 
 	async onload() {
 		console.log('loading plugin: ' + plugin_name);
 
-		// await this.loadSettings();
+		this.registerView(
+			view_type,
+			(leaf) => (this.view = new RootLeavesListView(leaf, this))
+		);
 
-		// this.addRibbonIcon('dice', 'Sample Plugin', () => {
-		// 	new Notice('This is a notice!');
-		// });
+		if (this.app.workspace.layoutReady) {
+			this.initView();
+		} else {
+			this.registerEvent(this.app.workspace.on('layout-ready', this.initView));
+		}
 
-		// this.addStatusBarItem().setText('Status Bar Text');
+		this.registerEvent(this.app.workspace.on('active-leaf-change', this.handleFocusChange));
 
 		this.addCommand({
-			id: 'tinker-modal',
-			name: 'tinker modal',
-			// callback: () => {
-			// 	console.log('Simple Callback');
-			// },
-			checkCallback: (checking: boolean) => {
-				let leaf = this.app.workspace.activeLeaf;
-				if (leaf) {
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-					return true;
-				}
-				return false;
-			}
+			id: 'leaves-iterate',
+			name: 'leaves-iterate',
+			hotkeys: [{"modifiers": ["Alt"],"key": "y"}],
+			callback: () => this.leavesIterate(),
 		});
 
-		// this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addCommand({
+			id: 'placeholder',
+			name: 'placeholder',
+			hotkeys: [{ "modifiers": ["Alt"], "key": "k" }],
+			callback: () => this.placeholder(),
+		});
 
-		// this.registerCodeMirror((cm: CodeMirror.Editor) => {
-		// 	console.log('codemirror', cm);
-		// });
+	}
 
-		// this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-		// 	console.log('click', evt);
-		// });
+	private readonly initView = (): void => {
+		if (this.app.workspace.getLeavesOfType(view_type).length) {
+			return;
+		}
+		this.app.workspace.getLeftLeaf(false).setViewState({
+			type: view_type,
+			active: true,
+		});
+	};
 
-		// this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+	leavesIterate() {
+		this.app.workspace.iterateAllLeaves((leaf: any) => {
+			console.log(leaf.getDisplayText());
+			console.log(leaf.getViewState());
+		});
+	}
+
+	placeholder() {
+		let leaf_curr = this.app.workspace.activeLeaf;
+		let varm = this.app.workspace.createLeafBySplit(leaf_curr, 'horizontal', true);
+		console.log(varm);
+	}
+
+	private readonly handleFocusChange = async() => {
+		this.view.initialize();
 	}
 
 	onunload() {
 		console.log('unloading plugin: ' + plugin_name);
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+}
+
+
+class RootLeavesListView extends ItemView{
+	private readonly plugin: MyPlugin
+	
+	constructor(
+		leaf: WorkspaceLeaf,
+		plugin: MyPlugin,
+	) {
+		super(leaf);
+
+		this.plugin = plugin;
+		this.initialize();
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
+	public readonly initialize = (): void => {
+		let leaf_active = this.app.workspace.activeLeaf;
+		const rootEl = createDiv({ cls: 'nav-folder mod-root' });
+		const childrenEl = rootEl.createDiv({ cls: 'nav-folder-children' });
+		this.app.workspace.iterateRootLeaves((leaf: any) => {
+			const navFile = childrenEl.createDiv({ cls: 'nav-file' });
+			const navFileTitle = navFile.createDiv({ cls: 'nav-file-title' });
+
+			if (leaf === leaf_active) {
+				navFileTitle.addClass('is-active');
+			}
+
+			navFileTitle.createDiv({
+				cls: 'nav-file-title-content',
+				text: leaf.getDisplayText(),
+			});
+			const contentEl = this.containerEl.children[1];
+			contentEl.empty();
+			contentEl.appendChild(rootEl);
+		});
+	}
+
+	public getViewType(): string {
+		return view_type;
+	}
+
+	public getDisplayText(): string {
+		return 'Root Leaves';
+	}
+
+	public getIcon(): string {
+		return 'double-down-arrow-glyph';
+	}
+
+	// mch_todo change icons
+	public onHeaderMenu(menu: Menu): void {
+		menu
+			.addItem((item) => {
+				item
+					.setTitle('action1')
+					.setIcon('sweep')
+					.onClick(async () => {
+						new Notice('action1');
+					});
+			})
+			.addItem((item) => {
+				item
+					.setTitle('close')
+					.setIcon('cross')
+					.onClick(() => {
+						this.app.workspace.detachLeavesOfType(view_type);
+					});
+			});
 	}
 }
 
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('!Woah!');
-	}
-
-	onClose() {
-		let {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-
-// class SampleSettingTab extends PluginSettingTab {
-// 	plugin: MyPlugin;
-
-// 	constructor(app: App, plugin: MyPlugin) {
-// 		super(app, plugin);
-// 		this.plugin = plugin;
-// 	}
-
-// 	display(): void {
-// 		let {containerEl} = this;
-
-// 		containerEl.empty();
-
-// 		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-// 		new Setting(containerEl)
-// 			.setName('Setting #1')
-// 			.setDesc('It\'s a secret')
-// 			.addText(text => text
-// 				.setPlaceholder('Enter your secret')
-// 				.setValue('')
-// 				.onChange(async (value) => {
-// 					console.log('Secret: ' + value);
-// 					this.plugin.settings.mySetting = value;
-// 					await this.plugin.saveSettings();
-// 				}));
-// 	}
-// }
